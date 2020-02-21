7219e2625ae847116d96859561543216ac17068a
Mikle <bastazf@mail.ru>
2020-02-21 15:21:01 +0300

    created DAO object for wiki page

--- coreService/db_api.py
+++ coreService/db_api.py
@@ -1,27 +1,16 @@
 from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure
+from pymongo.errors import ConnectionFailure, WriteError, CollectionInvalid 
 from collections import OrderedDict
 import json
 from datetime import datetime
-
 import os
 from dotenv import load_dotenv
+
+
 # doesn't override any env variables, just set them from .env file if they don't exist
 load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
-
-
-# wiki page example for testing
-val_post = {
-            "name" : "val_post1",
-            "description" : "val_post1",
-            "tags" : [],
-            "text" : "text",
-            "creation_date" : datetime.utcnow(),
-            "synonyms" : [],
-            "relations" : [],
-            "attachments" : [{"content_type" : "image/png", "content" : b"sdfsdf"}]
-            }
+SCHEMA_PATH = os.getenv('SCHEMA_PATH')
 
 
 def init_db_client() -> MongoClient:
@@ -50,7 +39,7 @@ def client_availabe(client) -> bool:
     return ans
 
 
-def set_modification(**changes) -> dict:
+def set_modification(changes : dict) -> dict:
     '''
     changes : <key> : <values>
     changes - changes which should be applied on document
@@ -59,71 +48,90 @@ def set_modification(**changes) -> dict:
     return {"$set" : changes}
 
 
-def insert(collection, data : dict):
-    ''' on success returns ObjecId of inserted object '''
-    result = collection.insert_one(data)
-    return result.inserted_id
+class WikiPageDAO:
+    def __init__(self):
+        self.db_client = init_db_client()
+        self.database = self.db_client.wikiDB
+        try:
+            self.database.create_collection("wikipage_collection")
+            self.collection = self.database.wikipage_collection
+        except CollectionInvalid: # means that collection exists
+            self.collection = self.database.wikipage_collection
+        set_validation(self.database, "wikipage_collection", SCHEMA_PATH)
+        assert client_availabe(self.db_client) # raise error if can't conenct to db
 
 
-def delete(collection, filter : dict) -> int:
-    ''' returns amount of deleted objects '''
-    result = collection.delete_many(filter)
-    return result.deleted_count
+    def get(self, filter : dict = None, projection : list = None) -> list:
+        '''
+        filter : an object specifying elements which must be present for a document
+        to be included in the result set, if None - returns whole collection
+        projection (optional) : a list of field names that should be returned in the result set
 
+        returns a list of documents
+        '''
+        result = list(self.collection.find(filter, projection=projection))
+        return result
 
-def update(collection, modification, filter) -> int:
-    ''' returns amount of modificated documents '''
-    result = collection.update_many(update=modification, filter=filter)
-    return result.modified_count
+    def create(self, data : dict):
+        ''' 
+        on success returns ObjecId of inserted object,
+        if fails returns -1 
+        '''
+        try:
+            result = self.collection.insert_one(data)
+            return result.inserted_id
+        except WriteError:
+            return -1
 
+    def update(self, modification : dict, filter : dict) -> int:
+        ''' returns amount of modificated documents '''
+        result = self.collection.update_many(update=modification, filter=filter)
+        return result.modified_count
 
-def find(collection, filter : dict = None, projection : list = None) -> list:
-    '''
-    filter : an object specifying elements which must be present for a document
-    to be included in the result set, if None - returns whole collection
-    projection (optional) : a list of field names that should be returned in the result set
-
-    returns a list of documents
-    '''
-    return list(collection.find(filter, projection=projection))
+    def delete(self, filter : dict) -> int:
+        ''' returns amount of deleted objects '''
+        result = self.collection.delete_many(filter)
+        return result.deleted_count
 
 
 if __name__ == "__main__":
-    client = init_db_client()
-    assert client_availabe(client)
+    # wiki page example for testing
+    val_post = {
+            "name" : "val_post1",
+            "description" : "val_post1",
+            "tags" : [],
+            "text" : "text",
+            "creation_date" : datetime.utcnow(),
+            "synonyms" : [],
+            "relations" : [],
+            "attachments" : [{"content_type" : "image/png", "content_data" : b"sdfsdf"}]
+            }
 
-    db = client.test_database
-
-    # drop collection if it exists
-    db.test_collection.drop()
-    db.create_collection("test_collection")
-    collection = client.test_database.test_collection
-
-    set_validation(db, "test_collection", "schema.json")
+    DAO_obj = WikiPageDAO()
 
     print("Print whole collection")
-    print(find(collection))
+    print(DAO_obj.get())
 
     print("Inserting")
-    print(insert(collection, val_post))
+    print(DAO_obj.create(val_post))
 
     print("Find inserted obj")
-    print(find(collection, {"name" : "val_post1"}))
+    print(DAO_obj.get({"name" : "val_post1"}))
 
     print("Update inserted obj")
-    print(update(collection, set_modification(name="updated_post1"), {"name" : "val_post1"}))
+    print(DAO_obj.update(set_modification({"name" : "updated_post1"}), {"name" : "val_post1"}))
 
     print("Find wrong obj")
-    print(find(collection, {"name" : "val_post1"}))
+    print(DAO_obj.get({"name" : "val_post1"}))
 
     print("Find correct obj, get name")
-    print(find(collection, {"name" : "updated_post1"}, ["name"]))
+    print(DAO_obj.get({"name" : "updated_post1"}, ["name"]))
 
     print("Delete wrong obj")
-    print(delete(collection, {"name" : "wrong_name"}))
+    print(DAO_obj.delete({"name" : "wrong_name"}))
 
     print("Delete correct obj")
-    print(delete(collection, {"name" : "updated_post1"}))
+    print(DAO_obj.delete({"name" : "updated_post1"}))
 
     print("Print whole collection")
-    print(find(collection))
+    print(DAO_obj.get())
