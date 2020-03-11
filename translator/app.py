b2a8df0546786fb6673b9e89f5c8a2f1a1dd0553
raxel <leha.ust@mail.ru>
2020-03-11 10:58:06 +0000

    Added translating for description field

--- translator/app.py
+++ translator/app.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import time
+from itertools import cycle
 
 API_KEY = 'trnsl.1.1.20200309T121046Z.10fd25429d4264e1.1b1d8883b0e564975a110cb11b558d5d1e1aa68b'
 API_ADDRESS = 'http://192.168.0.111:5000/api/wiki'
@@ -15,41 +16,45 @@ def translate(word, lang="en-ru"):
         return None
 
 
-def get():
-    response = requests.get(API_ADDRESS, params={'russian_name':''}, headers={'X-Fields':'_id, name, russian_name'})
+def get(field_name):
+    response = requests.get(API_ADDRESS, params={'russian_%s' % field_name:''},
+            headers={'X-Fields':'_id, {0}, russian_{0}'.format(field_name)})
     return json.loads(response.text)
 
 
-def translate_json_data(json_data):
+def translate_json_data(json_data, field_name):
     """json data is a list of dicts"""
     for obj in json_data:
-        if not obj.get('russian_name'):
-            obj['russian_name'] = translate(obj['name'], lang="en-ru")
+        if not obj.get('russian_%s' % field_name):
+            obj['russian_%s' % field_name] = translate(obj[field_name], lang="en-ru")
             time.sleep(1) #to not get ban
     return json_data
 
 
-def put(json_data):
+def put(json_data, field_name):
     for obj in json_data:
-        res = requests.put(API_ADDRESS, params={"_id" : obj.get('_id')}, data=json.dumps({'russian_name':obj.get('russian_name')}),
-            headers={'Content-Type': 'application/json', "accept": "application/json"})
+        res = requests.put(API_ADDRESS, params={"_id" : obj.get('_id')},
+                data=json.dumps({'russian_%s' % field_name:obj.get('russian_%s' % field_name)}),
+                    headers={'Content-Type': 'application/json', "accept": "application/json"})
 
 
-def __format_print(json_data):
+def __format_print(json_data, field_name):
     """Used for testing"""
     print("len = %d:" % len(json_data) )
     for i in json_data:
-        print("id: %s;\tname: %s;\trus: %s" % (i['_id'], i['name'], i.get('russian_name')))
+        print("id: %s;\tname: %s;\trus: %s" % (i['_id'], i[field_name], i.get('russian_%s' % field_name)))
 
 
 if __name__=="__main__":
+    field_name_iterator = cycle(("name", "description"))
     while(True):
         try:
-            data = get()
+            field_name = next(field_name_iterator)
+            data = get(field_name)
             if data:
-                data = translate_json_data(data)
-                put(data)
-                print("Translated %d words" % len(data))
+                data = translate_json_data(data, field_name)
+                put(data, field_name)
+                print("Translated %d %s fields" % (len(data), field_name))
         except Exception:
             print("Can't connect to DB")
         time.sleep(10)
