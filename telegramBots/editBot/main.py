26cc08ff84fbfd00a2595cb20c46c43c76e7ba49
Abrash Mikhail <bastazf@mail.ru>
2020-03-26 23:58:39 +0300

    added text fields editing

--- telegramBots/editBot/main.py
+++ telegramBots/editBot/main.py
@@ -3,9 +3,13 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher import FSMContext
-from config import TG_TOKEN, PROXY_AUTH, PROXY_URL, get_from_wiki
+from config import TG_TOKEN, PROXY_AUTH, PROXY_URL, get_from_wiki, update_in_wiki
+from utils import *
 import re
 import logging
+from itertools import islice
+import io
+import json
 
 
 # Initialize bot and dispatcher
@@ -15,6 +19,8 @@ else:
     bot = Bot(token=TG_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+
+ARRAY_FIELDS = ["synonyms", "tags", "relations"]
 
 
 def get_replymarkup_names(names):
@@ -29,6 +35,20 @@ def get_replymarkup_yesno():
     markup.add("Yes", "No")
     markup.add("Cancel")
     return markup
+
+def get_replymarkup_fields(word):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    for fields in split_every(2, word):
+        markup.add(*fields)
+    markup.add("Cancel")
+    return markup
+
+def split_every(n, iterable):
+    i = iter(iterable)
+    piece = list(islice(i, n))
+    while piece:
+        yield piece
+        piece = list(islice(i, n))
 
 # States
 class EditProcess(StatesGroup):
@@ -54,7 +74,6 @@ async def cancel_handler(message: types.Message, state: FSMContext):
     await message.reply('Cancelled.', reply_markup=types.ReplyKeyboardRemove())
 
 
-
 @dp.message_handler(commands=["start", "info"])
 async def start(message: types.Message):
     await message.answer('Enter id or name of object you want to edit')
@@ -63,7 +82,7 @@ async def start(message: types.Message):
 @dp.message_handler(state='*', regexp='^[i|I][d|D]\s*:?\s*([0-9a-z]{24})')
 async def find_by_id(message: types.Message, state: FSMContext):
     _id = re.match("^[i|I][d|D]\s*:?\s*([0-9a-z]{24})", message.text).group(1)
-    res = get_from_wiki(id=_id)["_id"]
+    res = get_from_wiki(id=_id, ret_fields=["name"])["_id"]
     if res:
         async with state.proxy() as idp:
             idp["_id"] = res["_id"]
@@ -72,10 +91,10 @@ async def find_by_id(message: types.Message, state: FSMContext):
         await message.answer("Are you sure want to edit this one?", reply_markup=get_replymarkup_yesno())
 
 
-@dp.message_handler(state='*', regexp='^[n|N]ame\s*:?\s*(.*)')
+@dp.message_handler(state='*', regexp='^[n|N]ame\s*:?\s*(.+)')
 async def find_by_name(message: types.Message, state: FSMContext):
-    name = re.match("^[n|N]ame\s*:?\s*(.*)", message.text).group(1)
-    res = get_from_wiki(name=name)["name"]
+    name = re.match("^[n|N]ame\s*:?\s*(.+)", message.text).group(1)
+    res = get_from_wiki(name=name, ret_fields=["name"])["name"]
     if len(res) == 0:
         await message.answer(f"Nothing found for {name}")
         return
@@ -92,17 +111,101 @@ async def process_name(message: types.Message, state: FSMContext):
     await EditProcess._id.set()
     await message.answer("Are you sure want to edit this one?", reply_markup=get_replymarkup_yesno())
 
+
 @dp.message_handler(state=EditProcess._id)
-async def process_name(message: types.Message, state: FSMContext):
+async def process_id(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         if message.text == 'Yes':
             await EditProcess.old_field.set()
             logging.info(f"Yes to {data['_id']}")
-            # send choose field markup
+            word_info = get_from_wiki(id=data['_id'])["_id"]
+            del word_info["_id"]
+            await message.answer("What field you wish to change?", reply_markup=get_replymarkup_fields(word_info))
         elif message.text == 'No':
             logging.info(f"NO to {data['_id']}")
             await state.finish()
             await message.answer('Ok', reply_markup=types.ReplyKeyboardRemove())
+
+
+
+@dp.message_handler(state=EditProcess.old_field)
+async def process_field(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data["old_field"] = message.text
+    await EditProcess.new_field.set()
+    await message.answer(f"You have chosen to edit field '{message.text}'. Send new value for this.", reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(state=EditProcess.new_field, content_types=["audio"])
+async def get_new_field_value(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        _id = data["_id"]
+        old_field = data["old_field"]
+    if old_field != "attachments":
+        await message.answer(f"You can't add media to this field. Canceled.")
+        await state.finish()
+        return
+    audio_info = json.loads(message.audio.as_json())
+    audio_data = io.BytesIO()
+    await message.audio.download(audio_data)
+    audio_item = {
+        "content_type" : audio_info["mime_type"],
+        "content_data" : bytes_to_str(audio_data.read()),
+        "descritpion" : audio_info["title"]
+    }
+    res = update_in_wiki(_id, {old_field : [audio_item]})
+    await state.finish()
+    if res == 1:
+        await message.answer(f"You have succesfully updated field '{old_field}'.")
+    else:
+        await message.answer(f"Something went wrong, field is not updated.")
+
+
+@dp.message_handler(state=EditProcess.new_field, content_types=["photo"])
+async def get_new_field_value(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        _id = data["_id"]
+        old_field = data["old_field"]
+    if old_field != "attachments":
+        await message.answer(f"You can't add media to this field. Canceled.")
+        await state.finish()
+        return
+    new_val = []
+    for photo in message.photo:
+        photo_info = json.loads(photo.as_json())
+        photo_data = io.BytesIO()
+        await photo.download(photo_data)
+        photo_item = {
+            "content_type" : "image/jpeg",#photo_info["mime_type"],
+            "content_data" : bytes_to_str(photo_data.read()),
+            # "descritpion" : photo_info["title"]
+        }
+        new_val.append(photo_item)
+    res = update_in_wiki(_id, {old_field : new_val})
+    await state.finish()
+    if res == 1:
+        await message.answer(f"You have succesfully updated field '{old_field}'.")
+    else:
+        await message.answer(f"Something went wrong, field is not updated.")
+
+
+@dp.message_handler(state=EditProcess.new_field, content_types=["text"])
+async def get_new_field_value(message: types.Message, state: FSMContext):
+    global ARRAY_FIELDS
+    async with state.proxy() as data:
+        _id = data["_id"]
+        old_field = data["old_field"]
+    if old_field in ARRAY_FIELDS:
+        new_val = [x.strip() for x in message.text.strip().lstrip("[").rstrip("]").split(",")]
+    else:
+        new_val = message.text
+    res = update_in_wiki(_id, {old_field : new_val})
+    await state.finish()
+    if res == 1:
+        await message.answer(f"You have succesfully updated field '{old_field}'.")
+    else:
+        await message.answer(f"Something went wrong, field is not updated.")
+
 
 @dp.message_handler()
 async def unexpected(message: types.Message):
