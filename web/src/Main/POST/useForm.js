906ce93c4f1014b62a2628514f60aa710c3a6ed8
kirillr123 <wannabefree232@gmail.com>
2020-04-18 01:55:27 +0300

    Issue #61 add multiple images

--- web/src/Main/POST/useForm.js
+++ web/src/Main/POST/useForm.js
@@ -5,7 +5,9 @@ import { useHistory } from "react-router-dom";
 
 function useForm(callback) {
  const [dbm, setDbm] = useState(new DatabaseModel());
+ const [preview, setPreview] = useState([])
  let history = useHistory();
+ dbm["attachments"] = [];
 
  const handleSubmit = (event) => {
     if (event) {
@@ -19,49 +21,47 @@ function useForm(callback) {
         dbm["synonyms"] = [];
         dbm["relations"] = []; 
 
-        //here async file upload starts
-        dbm["attachments"] = [];
+        //just sends whatever is in dbm item
+        let globName = "188.124.37.185";
+        let apiUrl = "/api/wiki";
 
-        const file = document.querySelector('input[type=file]').files[0];
-        const reader = new FileReader();
-        reader.addEventListener("load", function () {
+        fetch("http://" + globName + ":5000" + apiUrl, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(dbm) // body data type must match "Content-Type" header
+        }).then((data) => {
+          console.log(data.status);
+          if([200,201,202,203,204,205].includes(data.status)){
+          alert("Data sent successfully!");
+          history.push('/get');} 
+          else if([400,401,402,403,404,405].includes(data.status)){
+          alert("Client error!");} 
+          else if([500,501,502,503,504,505].includes(data.status)){
+          alert("Server error!");}
+        })
+        }
+  };
+    
+  const handleFileSelect = (event) => {
+    setPreview([... preview, URL.createObjectURL(event.target.files[0])]);
+    const file = document.querySelector('input[type=file]').files[0];
+    const reader = new FileReader();
+    reader.addEventListener("load", function () {
           // here encoding to base64 happens
           // we need to wait until file is ready and THEN we send data
           dbm.addAttachment(new Attachment(file.type, reader.result.replace(/data.*base64,/, "")));
-          SendData();
-        }, false);
+          console.log(file.name);
+    }, false);
        
-        if (file) {
-         reader.readAsDataURL(file);
-        } else {
-          SendData();
-        }
-    }
-  };
-
-  function SendData(){
-    //just sends whatever is in dbm item
-    let globName = "188.124.37.185";
-    let apiUrl = "/api/wiki";
-
-    fetch("http://" + globName + ":5000" + apiUrl, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(dbm) // body data type must match "Content-Type" header
-    }).then((data) => {
-      console.log(data.status);
-      if([200,201,202,203,204,205].includes(data.status)){
-      alert("Data sent successfully!");
-      history.push('/get');} 
-      else if([400,401,402,403,404,405].includes(data.status)){
-      alert("Client error!");} 
-      else if([500,501,502,503,504,505].includes(data.status)){
-      alert("Server error!");}
-      
-  })}
-
+    if (file) {
+      reader.readAsDataURL(file);
+    } 
+  }
+  
   return {
-    handleSubmit
+    handleSubmit,
+    handleFileSelect,
+    preview
   };
 }
 
