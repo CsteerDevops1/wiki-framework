bbdfb15ebf00a99956eee6dcd5147262e434a968
Abrash Mikhail <bastazf@mail.ru>
2020-05-13 20:47:54 +0300

    #98 added ProxyFix

--- coreService/main.py
+++ coreService/main.py
@@ -7,10 +7,14 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from wiki_api.api_app import app as api_application, HOST, PORT
 from authorization.auth_app import app as auth_application
 from werkzeug.serving import run_simple
+from werkzeug.contrib.fixers import ProxyFix
+
 
 middleware = DispatcherMiddleware(api_application, {
     '/api/wiki/auth': auth_application,
 })
+
+middleware = ProxyFix(middleware)
 
 # not recommended for production
 run_simple(HOST, int(PORT), middleware)
